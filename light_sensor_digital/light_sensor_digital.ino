83a24baf84c27048353bd1b3ca14b0276e92ce62
DuYeong_Choi <endud74@naver.com>
2018-12-05 15:29:52 +0900

    lightSensor
    조도센서 이용한 LED, 피에조, 디지털화

---
+++ light_sensor_digital/light_sensor_digital.ino
@@ -0,0 +1,14 @@
+int led_pin = 13;
+void setup() {
+}
+
+void loop() {
+  int light = analogRead(A0);
+  int ledLight = map(light, 0, 1023, 255, 0);
+
+  if (light <= 500) {
+    digitalWrite(led_pin, HIGH);
+  } else {
+    digitalWrite(led_pin, LOW);
+  }
+}
